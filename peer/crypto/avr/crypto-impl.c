16365ec5ce5c2377a799c5f5783c3cc55a37990a
Mattias Buelens <mattias.buelens@student.kuleuven.be>
2015-08-03 00:55:14 +0200

    Added crypto API implementation for AVR.

---
+++ peer/crypto/avr/crypto-impl.c
@@ -0,0 +1,52 @@
+/**
+ * \file
+ *         Cryptographic implementations for AVR
+ * \author
+ *         Mattias Buelens <mattias.buelens@student.kuleuven.be>
+ */
+
+#include "common.h"
+#include "crypto-impl.h"
+
+void sha1_reset(sha1_context_t *context) {
+	sha1_init(&context->state);
+	context->next_length = 0;
+}
+
+bool sha1_add(sha1_context_t *context, const uint8_t *src, size_t len) {
+	// First fill up next block if non-empty
+	if (context->next_length != 0) {
+		// Append data to next block buffer
+		int_least16_t copy_len = MIN(len,
+				SHA1_BLOCK_BYTES - context->next_length);
+		memcpy(&context->next_block[context->next_length], src, copy_len);
+		context->next_length += copy_len;
+		src += copy_len;
+		len -= copy_len;
+		// If next block is complete
+		if (context->next_length == SHA1_BLOCK_BYTES) {
+			// Process next block
+			sha1_nextBlock(&context->state, context->next_block);
+			context->next_length = 0;
+		}
+	}
+	// Process full blocks
+	while (len >= SHA1_BLOCK_BYTES) {
+		sha1_nextBlock(&context->state, src);
+		src += SHA1_BLOCK_BYTES;
+		len -= SHA1_BLOCK_BYTES;
+	}
+	// Store remainder in next block buffer
+	if (len > 0) {
+		memcpy(context->next_block, src, len);
+		context->next_length = len;
+	}
+	return true;
+}
+
+bool sha1_result(sha1_context_t *context, sha1_digest_t *dest) {
+	sha1_lastBlock(&context->state, context->next_block,
+			context->next_length * 8);
+	sha1_ctx2hash(&dest->bytes, &context->state);
+	return true;
+}
---
+++ peer/crypto/avr/crypto-impl.h
@@ -0,0 +1,28 @@
+/**
+ * \file
+ *         Cryptographic implementations for AVR
+ * \author
+ *         Mattias Buelens <mattias.buelens@student.kuleuven.be>
+ */
+
+#ifndef NANOTORRENT_CRYPTO_IMPL_H_
+#define NANOTORRENT_CRYPTO_IMPL_H_
+
+#include "sha1.h"
+
+/*
+ * SHA1 context
+ */
+typedef struct sha1_context {
+	/**
+	 * Internal state
+	 */
+	sha1_ctx_t state;
+	/**
+	 * Next message block
+	 */
+	int_least16_t next_length;
+	uint8_t next_block[SHA1_BLOCK_BYTES];
+} sha1_context_t;
+
+#endif /* NANOTORRENT_CRYPTO_IMPL_H_ */
